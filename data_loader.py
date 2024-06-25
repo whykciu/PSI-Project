1c463f884fb3aca87f5d070cf266ef62e0623191
whykciu <smieszek1567@gmail.com>
2024-06-25 11:10:22 +0200

    Add data_loader.py

---
+++ data_loader.py
@@ -0,0 +1,59 @@
+import yfinance as yf
+
+def compute_ratio(data, window):
+        return data['Close'] / data['Close'].rolling(window=window).mean()
+
+def compute_trend(data, window):
+    return data["Target"].shift(1).rolling(window=window).sum()
+
+def compute_rsi(data, window):
+    delta = data['Close'].diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+def compute_sma(data, window):
+    return data['Close'].rolling(window=window).mean()
+
+def compute_ema(data, window):
+    return data['Close'].ewm(span=window, adjust=False).mean()
+
+def compute_macd(data, short_window, long_window, signal_window):
+    short_ema = data['Close'].ewm(span=short_window, adjust=False).mean()
+    long_ema = data['Close'].ewm(span=long_window, adjust=False).mean()
+    macd = short_ema - long_ema
+    signal = macd.ewm(span=signal_window, adjust=False).mean()
+    return macd, signal
+
+def load():
+    data = yf.Ticker("AAPL")
+    data = data.history(period="max")
+    del data["Dividends"]
+    del data["Stock Splits"]
+    data["Tomorrow"] = data["Close"].shift(-1)
+    data = data.loc["1996-01-01":]
+    data["Target"] = (data["Tomorrow"] > data["Close"]).astype(int)
+    predictors = ["Open", "High", "Low", "Close", "Volume"]
+    
+    windows = [2, 5, 10, 50, 100, 300, 500, 1000]
+
+    for window in windows:
+        ratio_column = f"Close_Ratio_{window}"
+        trend_column = f"Trend_{window}"
+        rsi_column = f"RSI_{window}"
+        sma_column = f"SMA_{window}"
+        ema_column = f"EMA_{window}"
+        macd_column = f"MACD_{window}"
+        signal_column = f"Signal_{window}"
+        data.loc[:, ratio_column] = compute_ratio(data, window)
+        data.loc[:, trend_column] = compute_trend(data, window)
+        data.loc[:, rsi_column] = compute_rsi(data, window)
+        data.loc[:, sma_column] = compute_sma(data, window)
+        data.loc[:, ema_column] = compute_ema(data, window)
+        data.loc[:, macd_column], data.loc[:, signal_column] = compute_macd(data, window, window * 2, int(window * 1.5))
+        predictors += [ratio_column, trend_column, rsi_column, sma_column, ema_column, macd_column, signal_column]
+
+    data = data.dropna()
+    return data, predictors
--- stock_market_pred.py
+++ stock_market_pred.py
@@ -5,64 +5,8 @@ from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_sc
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier, AdaBoostClassifier
 from xgboost import XGBClassifier
 import joblib
-
-data = yf.Ticker("AAPL")
-data = data.history(period="max")
-data
-data.plot(y="Close")
-del data["Dividends"]
-del data["Stock Splits"]
-data["Tomorrow"] = data["Close"].shift(-1)
-data = data.loc["1996-01-01":]
-data.plot(y="Close")
-data["Target"] = (data["Tomorrow"] > data["Close"]).astype(int)
-data
-predictors = ["Open", "High", "Low", "Close", "Volume"]
-def compute_ratio(data, window):
-    return data['Close'] / data['Close'].rolling(window=window).mean()
-
-def compute_trend(data, window):
-    return data["Target"].shift(1).rolling(window=window).sum()
-
-def compute_rsi(data, window):
-    delta = data['Close'].diff()
-    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
-    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
-    rs = gain / loss
-    rsi = 100 - (100 / (1 + rs))
-    return rsi
-
-def compute_sma(data, window):
-    return data['Close'].rolling(window=window).mean()
-
-def compute_ema(data, window):
-    return data['Close'].ewm(span=window, adjust=False).mean()
-
-def compute_macd(data, short_window, long_window, signal_window):
-    short_ema = data['Close'].ewm(span=short_window, adjust=False).mean()
-    long_ema = data['Close'].ewm(span=long_window, adjust=False).mean()
-    macd = short_ema - long_ema
-    signal = macd.ewm(span=signal_window, adjust=False).mean()
-    return macd, signal
-windows = [2, 5, 10, 50, 100, 300, 500, 1000]
-
-for window in windows:
-    ratio_column = f"Close_Ratio_{window}"
-    trend_column = f"Trend_{window}"
-    rsi_column = f"RSI_{window}"
-    sma_column = f"SMA_{window}"
-    ema_column = f"EMA_{window}"
-    macd_column = f"MACD_{window}"
-    signal_column = f"Signal_{window}"
-    data.loc[:, ratio_column] = compute_ratio(data, window)
-    data.loc[:, trend_column] = compute_trend(data, window)
-    data.loc[:, rsi_column] = compute_rsi(data, window)
-    data.loc[:, sma_column] = compute_sma(data, window)
-    data.loc[:, ema_column] = compute_ema(data, window)
-    data.loc[:, macd_column], data.loc[:, signal_column] = compute_macd(data, window, window * 2, int(window * 1.5))
-    predictors += [ratio_column, trend_column, rsi_column, sma_column, ema_column, macd_column, signal_column]
-data = data.dropna()
-data
+from data_loader import load
+data, predictors = load()
 train_data, test_data = train_test_split(data, test_size=0.2, shuffle=False)
 
 X_train, y_train = train_data[predictors], train_data["Target"]
